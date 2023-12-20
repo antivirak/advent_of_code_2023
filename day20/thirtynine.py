2628493dff5146285a5abea98e7f8b761fe3dd6a
cerman <jaroslav.cerman@merck.com>
2023-12-21 00:20:09 +0100

    Working with test3, but still not real data

--- day20/thirtynine.py
+++ day20/thirtynine.py
@@ -12,7 +12,7 @@ START = 'broadcaster'
 
 def main() -> int:
     """main"""
-    with open('test_input2.txt', 'r') as f_in:
+    with open('input.txt', 'r') as f_in:
         modules = f_in.readlines()
 
     # construct dict of all modules adjacency
@@ -29,21 +29,24 @@ def main() -> int:
             'state': False if module_type == FLIP else 'low',
             'next': [node.strip() for node in next_items.split(', ')]
         }
+    #for module, data in adjacency.items():
+    #    if len(set(data['next'])) != len(data['next']):
+    #        raise ValueError(f'Node {module} has duplicate next nodes')
     for module, data in adjacency.items():
         if data['type'] == CONJUNCTION:
             adjacency[module]['state'] = {
                 ancestor: 'low' for ancestor in [
                     key for key, val in adjacency.items() if module in val['next']
-                ]
+                ]  # if ancestor != START
             }
 
     from pprint import pprint
     pprint(adjacency)
-    # Simulate button push. Could be some kind of recursive BFS/DFS
+    # Simulate button push. Could be recursive BFS
     state_count = {'low': 0, 'high': 0}
     # state = 'low'
     for _ in range(1000):  # TODO find loops (then lcm?) or cache results
-        queue = deque(adjacency[START]['next'].copy())
+        queue = deque(tuple([START, node]) for node in adjacency[START]['next'])
         states = deque(['low' for _ in queue])
         # visited = set(queue)
         # visited = {node: 0 for node in adjacency.keys()}
@@ -53,7 +56,7 @@ def main() -> int:
         # print('initial state: ', state_count)
         while queue:
             # print(queue)
-            node = queue.popleft()
+            prev_node, node = queue.popleft()
             state = states.popleft()
             # visited.add(node)
             # visited[node] += 1
@@ -64,29 +67,28 @@ def main() -> int:
                 if adjacency[node]['state']:
                     state = 'high'
             elif adjacency[node]['type'] == CONJUNCTION:
-                # adjacency[node]['state'][prev_node] = state
+                adjacency[node]['state'][prev_node] = state
                 # print(adjacency[node]['state'])
                 state = 'low' if all(state_ == 'high' for state_ in adjacency[node]['state'].values()) else 'high'
                 # print(state)
             else:
                 raise ValueError(f'Unknown type {adjacency[node]["type"]}')
-            state_count[state] += len(adjacency[node]['next'])
-            # print('incrementing state ', state, ' from ', node, ' to ', adjacency[node]['next'])
-            # to_extend = set(adjacency[node]['next']) - set(key for key, val in visited.items() if val > 0)
             to_extend = adjacency[node]['next']  # set() - visited
-            queue.extend(item for item in to_extend if item in adjacency)
+            state_count[state] += len(to_extend)
+            print('incrementing state ', state, ' from ', node, ' to ', adjacency[node]['next'])
+            # to_extend = set(adjacency[node]['next']) - set(key for key, val in visited.items() if val > 0)
+            queue.extend(tuple([node, item]) for item in to_extend if item in adjacency)
             states.extend([state for _ in to_extend])
-            for next_ in adjacency[node]['next']:
-                if adjacency.get(next_, {}).get('type') == CONJUNCTION:
-                    adjacency[next_]['state'][node] = state  # TODO still does not work, maybe I'm using DFS instead of BFS. Add "time" sync?
-            # if any(adjacency.get(next_, {}).get('type') == CONJUNCTION for next_ in adjacency[node]['next']):
-            #     prev_node = node  # TODO this does not work for multiple conjunctions (real input)
+            # for next_ in adjacency[node]['next']:
+            #     if adjacency.get(next_, {}).get('type') == CONJUNCTION:
+            #         adjacency[next_]['state'][node] = state  # TODO still does not work. Add "time" sync?
+        #break
         #print(state_count)
         #if _ == 4:
         #    return prod(state_count.values())
 
     return prod(state_count.values())  # 688623195 too low
-    #                                    688623195
+    #                                    688580648
 
 
 if __name__ == '__main__':
