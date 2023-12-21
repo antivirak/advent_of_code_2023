ef08a984bdcf333f96a2e1298cb898a322bc253e
cerman <jaroslav.cerman@merck.com>
2023-12-21 11:46:22 +0100

    Part1 solution

--- day20/thirtynine.py
+++ day20/thirtynine.py
@@ -1,21 +1,122 @@
 """
---- 
+--- Pulse Propagation ---
+
+With your help, the Elves manage to find the right parts and fix all of the machines. Now, they just need to send the command to boot up the machines and get the sand flowing again.
+
+The machines are far apart and wired together with long cables. The cables don't connect to the machines directly, but rather to communication modules attached to the machines that perform various initialization tasks and also act as communication relays.
+
+Modules communicate using pulses. Each pulse is either a high pulse or a low pulse. When a module sends a pulse, it sends that type of pulse to each module in its list of destination modules.
+
+There are several different types of modules:
+
+Flip-flop modules (prefix %) are either on or off; they are initially off. If a flip-flop module receives a high pulse, it is ignored and nothing happens. However, if a flip-flop module receives a low pulse, it flips between on and off. If it was off, it turns on and sends a high pulse. If it was on, it turns off and sends a low pulse.
+
+Conjunction modules (prefix &) remember the type of the most recent pulse received from each of their connected input modules; they initially default to remembering a low pulse for each input. When a pulse is received, the conjunction module first updates its memory for that input. Then, if it remembers high pulses for all inputs, it sends a low pulse; otherwise, it sends a high pulse.
+
+There is a single broadcast module (named broadcaster). When it receives a pulse, it sends the same pulse to all of its destination modules.
+
+Here at Desert Machine Headquarters, there is a module with a single button on it called, aptly, the button module. When you push the button, a single low pulse is sent directly to the broadcaster module.
+
+After pushing the button, you must wait until all pulses have been delivered and fully handled before pushing it again. Never push the button if modules are still processing pulses.
+
+Pulses are always processed in the order they are sent. So, if a pulse is sent to modules a, b, and c, and then module a processes its pulse and sends more pulses, the pulses sent to modules b and c would have to be handled first.
+
+The module configuration (your puzzle input) lists each module. The name of the module is preceded by a symbol identifying its type, if any. The name is then followed by an arrow and a list of its destination modules. For example:
+
+broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a
+In this module configuration, the broadcaster has three destination modules named a, b, and c. Each of these modules is a flip-flop module (as indicated by the % prefix). a outputs to b which outputs to c which outputs to another module named inv. inv is a conjunction module (as indicated by the & prefix) which, because it has only one input, acts like an inverter (it sends the opposite of the pulse type it receives); it outputs to a.
+
+By pushing the button once, the following pulses are sent:
+
+button -low-> broadcaster
+broadcaster -low-> a
+broadcaster -low-> b
+broadcaster -low-> c
+a -high-> b
+b -high-> c
+c -high-> inv
+inv -low-> a
+a -low-> b
+b -low-> c
+c -low-> inv
+inv -high-> a
+After this sequence, the flip-flop modules all end up off, so pushing the button again repeats the same sequence.
+
+Here's a more interesting example:
+
+broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output
+This module configuration includes the broadcaster, two flip-flops (named a and b), a single-input conjunction module (inv), a multi-input conjunction module (con), and an untyped module named output (for testing purposes). The multi-input conjunction module con watches the two flip-flop modules and, if they're both on, sends a low pulse to the output module.
+
+Here's what happens if you push the button once:
+
+button -low-> broadcaster
+broadcaster -low-> a
+a -high-> inv
+a -high-> con
+inv -low-> b
+con -high-> output
+b -high-> con
+con -low-> output
+Both flip-flops turn on and a low pulse is sent to output! However, now that both flip-flops are on and con remembers a high pulse from each of its two inputs, pushing the button a second time does something different:
+
+button -low-> broadcaster
+broadcaster -low-> a
+a -low-> inv
+a -low-> con
+inv -high-> b
+con -high-> output
+Flip-flop a turns off! Now, con remembers a low pulse from module a, and so it sends only a high pulse to output.
+
+Push the button a third time:
+
+button -low-> broadcaster
+broadcaster -low-> a
+a -high-> inv
+a -high-> con
+inv -low-> b
+con -low-> output
+b -low-> con
+con -high-> output
+This time, flip-flop a turns on, then flip-flop b turns off. However, before b can turn off, the pulse sent to con is handled first, so it briefly remembers all high pulses for its inputs and sends a low pulse to output. After that, flip-flop b turns off, which causes con to update its state and send a high pulse to output.
+
+Finally, with a on and b off, push the button a fourth time:
+
+button -low-> broadcaster
+broadcaster -low-> a
+a -low-> inv
+a -low-> con
+inv -high-> b
+con -high-> output
+This completes the cycle: a turns off, causing con to remember only low pulses and restoring all modules to their original states.
+
+To get the cables warmed up, the Elves have pushed the button 1000 times. How many pulses got sent as a result (including the pulses sent by the button itself)?
+
+In the first example, the same thing happens every time the button is pushed: 8 low pulses and 4 high pulses are sent. So, after pushing the button 1000 times, 8000 low pulses and 4000 high pulses are sent. Multiplying these together gives 32000000.
+
+In the second example, after pushing the button 1000 times, 4250 low pulses and 2750 high pulses are sent. Multiplying these together gives 11687500.
+
+Consult your module configuration; determine the number of low pulses and high pulses that would be sent after pushing the button 1000 times, waiting for all pulses to be fully handled after each push of the button. What do you get if you multiply the total number of low pulses sent by the total number of high pulses sent?
 """
 
 from collections import deque
 from math import prod
+from typing import Iterable
 
 FLIP = '%'
 CONJUNCTION = '&'
 START = 'broadcaster'
 
 
-def main() -> int:
-    """main"""
-    with open('input.txt', 'r') as f_in:
-        modules = f_in.readlines()
-
-    # construct dict of all modules adjacency
+def construct_graph(modules: Iterable) -> dict:
+    """Return dict of all modules adjacency"""
     adjacency = {}
     for module in modules:
         module = module.strip()
@@ -29,66 +130,54 @@ def main() -> int:
             'state': False if module_type == FLIP else 'low',
             'next': [node.strip() for node in next_items.split(', ')]
         }
-    #for module, data in adjacency.items():
-    #    if len(set(data['next'])) != len(data['next']):
-    #        raise ValueError(f'Node {module} has duplicate next nodes')
+    # add default state for conjunctions
     for module, data in adjacency.items():
         if data['type'] == CONJUNCTION:
-            adjacency[module]['state'] = {
+            data['state'] = {  # mutable, can assign right away
                 ancestor: 'low' for ancestor in [
                     key for key, val in adjacency.items() if module in val['next']
-                ]  # if ancestor != START
+                ]
             }
 
-    from pprint import pprint
-    pprint(adjacency)
-    # Simulate button push. Could be recursive BFS
+    return adjacency
+
+
+def main() -> int:
+    """main"""
+    with open('input.txt', 'r') as f_in:
+        modules = f_in.readlines()
+
+    adjacency = construct_graph(modules)
+    # Simulate button push. Could be recursive BFS, I'm going with iterative
     state_count = {'low': 0, 'high': 0}
-    # state = 'low'
     for _ in range(1000):  # TODO find loops (then lcm?) or cache results
         queue = deque(tuple([START, node]) for node in adjacency[START]['next'])
         states = deque(['low' for _ in queue])
-        # visited = set(queue)
-        # visited = {node: 0 for node in adjacency.keys()}
-        # for node in queue:
-        #     visited[node] -= 1
+        # add signal from button to broadcaster and from broadcaster to all adjacent nodes
         state_count['low'] += 1 + len(queue)
-        # print('initial state: ', state_count)
         while queue:
-            # print(queue)
             prev_node, node = queue.popleft()
             state = states.popleft()
-            # visited.add(node)
-            # visited[node] += 1
             if adjacency[node]['type'] == FLIP:
                 if state == 'high':
-                    continue
+                    continue  # high signal dies at flip-flop
                 adjacency[node]['state'] = not adjacency[node]['state']
                 if adjacency[node]['state']:
                     state = 'high'
             elif adjacency[node]['type'] == CONJUNCTION:
+                # remember incoming signal
                 adjacency[node]['state'][prev_node] = state
-                # print(adjacency[node]['state'])
                 state = 'low' if all(state_ == 'high' for state_ in adjacency[node]['state'].values()) else 'high'
-                # print(state)
             else:
                 raise ValueError(f'Unknown type {adjacency[node]["type"]}')
-            to_extend = adjacency[node]['next']  # set() - visited
-            state_count[state] += len(to_extend)
-            print('incrementing state ', state, ' from ', node, ' to ', adjacency[node]['next'])
-            # to_extend = set(adjacency[node]['next']) - set(key for key, val in visited.items() if val > 0)
-            queue.extend(tuple([node, item]) for item in to_extend if item in adjacency)
-            states.extend([state for _ in to_extend])
-            # for next_ in adjacency[node]['next']:
-            #     if adjacency.get(next_, {}).get('type') == CONJUNCTION:
-            #         adjacency[next_]['state'][node] = state  # TODO still does not work. Add "time" sync?
-        #break
-        #print(state_count)
-        #if _ == 4:
-        #    return prod(state_count.values())
 
-    return prod(state_count.values())  # 688623195 too low
-    #                                    688580648
+            to_extend = adjacency[node]['next']
+            state_count[state] += len(to_extend)
+            # print('incrementing state ', state, ' from ', node, ' to ', to_extend)
+            queue.extend(tuple([node, item]) for item in to_extend if item in adjacency)
+            states.extend([state for item in to_extend if item in adjacency])
+
+    return prod(state_count.values())
 
 
 if __name__ == '__main__':
