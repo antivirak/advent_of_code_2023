c46d80006ef9b75686c39a52906fe360539693a3
cerman <jaroslav.cerman@merck.com>
2024-12-29 13:34:26 +0100

    test

--- day18/thirtyfive.py
+++ day18/thirtyfive.py
@@ -1,7 +1,9 @@
 """
 --- Lavaduct Lagoon ---
 
-Thanks to your efforts, the machine parts factory is one of the first factories up and running since the lavafall came back. However, to catch up with the large backlog of parts requests, the factory will also need a large supply of lava for a while; the Elves have already started creating a large lagoon nearby for this purpose.
+Thanks to your efforts, the machine parts factory is one of the first factories up and running since the lavafall came back.
+However, to catch up with the large backlog of parts requests, the factory will also need a large supply of lava for a while;
+the Elves have already started creating a large lagoon nearby for this purpose.
 
 However, they aren't sure the lagoon will be big enough; they've asked you to take a look at the dig plan (your puzzle input). For example:
 
@@ -19,9 +21,13 @@ R 2 (#7807d2)
 U 3 (#a77fa3)
 L 2 (#015232)
 U 2 (#7a21e3)
-The digger starts in a 1 meter cube hole in the ground. They then dig the specified number of meters up (U), down (D), left (L), or right (R), clearing full 1 meter cubes as they go. The directions are given as seen from above, so if "up" were north, then "right" would be east, and so on. Each trench is also listed with the color that the edge of the trench should be painted as an RGB hexadecimal color code.
+The digger starts in a 1 meter cube hole in the ground. They then dig the specified number of meters
+up (U), down (D), left (L), or right (R), clearing full 1 meter cubes as they go.
+The directions are given as seen from above, so if "up" were north, then "right" would be east, and so on.
+Each trench is also listed with the color that the edge of the trench should be painted as an RGB hexadecimal color code.
 
-When viewed from above, the above example dig plan would result in the following loop of trench (#) having been dug out from otherwise ground-level terrain (.):
+When viewed from above, the above example dig plan would result in the following loop of trench (#)
+having been dug out from otherwise ground-level terrain (.):
 
 #######
 #.....#
@@ -33,7 +39,8 @@ When viewed from above, the above example dig plan would result in the following
 ##..###
 .#....#
 .######
-At this point, the trench could contain 38 cubic meters of lava. However, this is just the edge of the lagoon; the next step is to dig out the interior so that it is one meter deep as well:
+At this point, the trench could contain 38 cubic meters of lava. However, this is just the edge of the lagoon;
+the next step is to dig out the interior so that it is one meter deep as well:
 
 #######
 #######
@@ -45,9 +52,11 @@ At this point, the trench could contain 38 cubic meters of lava. However, this i
 #######
 .######
 .######
-Now, the lagoon can contain a much more respectable 62 cubic meters of lava. While the interior is dug out, the edges are also painted according to the color codes in the dig plan.
+Now, the lagoon can contain a much more respectable 62 cubic meters of lava. While the interior is dug out,
+the edges are also painted according to the color codes in the dig plan.
 
-The Elves are concerned the lagoon won't be large enough; if they follow their dig plan, how many cubic meters of lava could it hold?
+The Elves are concerned the lagoon won't be large enough; if they follow their dig plan,
+how many cubic meters of lava could it hold?
 """
 
 from typing import List
